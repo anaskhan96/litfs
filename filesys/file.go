68bf9a5d2c7830c9dcc9656e91ea7af97cb85bdc
Anas Khan <anas.khan96@outlook.com>
2018-02-01 01:02:18 +0530

    fixing modes

--- filesys/file.go
+++ filesys/file.go
@@ -17,7 +17,7 @@ type File struct {
 func (file *File) Attr(ctx context.Context, attr *fuse.Attr) error {
 	log.Println("Attributes for file", file.Name)
 	attr.Inode = file.Inode
-	attr.Mode = 0776
+	attr.Mode = 0777
 	attr.Size = uint64(len(file.Data))
 	return nil
 }
