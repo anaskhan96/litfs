1ec30725be1b727b875cfa418b9662f395103760
Anas Khan <anas.khan96@outlook.com>
2018-03-13 09:30:17 +0530

    removing commented Destroy() function

--- filesys/fs.go
+++ filesys/fs.go
@@ -9,7 +9,3 @@ type FS struct {
 func (f *FS) Root() (fs.Node, error) {
 	return f.RootDir, nil
 }
-
-/*func (f *FS) Destroy() {
-	println("LOLLL why didn't I know about this function would've saved me so much timee")
-}*/
